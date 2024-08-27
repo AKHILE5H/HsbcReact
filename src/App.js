ee658057e2b50f398e2695eedb384895db20f8e3
AKHILE5H <akhileshingole07@gmail.com>
2024-08-27 10:17:39 +0530

    Added links to products

--- src/App.js
+++ src/App.js
@@ -10,12 +10,17 @@ import { Link, Route, Routes } from 'react-router-dom';
 function App() {
 
   return (
-    <div >
+    <div>
+    <div>
+    <img className='logo' src={require('./2560px-HSBC_logo_(2018).svg.png')} alt="HSBC Logo" />
+    </div>
     <div>
       <ul>
         <li className='nav'><Link to=''>Home</Link></li>
         <li className='nav'><Link to='topics'>Topics</Link></li>
         <li className='nav'><Link to='products'>Products</Link></li>
+        <li className='nav'><Link to='login'>Login</Link></li>
+        <li className='nav'><Link to='register'>Register</Link></li>
       </ul>
     </div>
     <div>
@@ -23,6 +28,8 @@ function App() {
         <Route path='' element={<Home/>}></Route>
         <Route path='topics' element={<Topics/>}></Route>
         <Route path='products' element={<Product/>}></Route>
+        <Route path='login' element={<Login/>}></Route>
+        <Route path='register' element={<Register/>}></Route>
       </Routes>
     </div>
     </div>
--- src/UI/products/product.js
+++ src/UI/products/product.js
@@ -1,5 +1,6 @@
 import './products.css'
 import{ps} from '../../service/productservice';
+import { Link } from 'react-router-dom';
 export function Product() {
 
     let data=ps.ProductService.search();
@@ -17,9 +18,9 @@ export function Product() {
                 <tbody>
                     {data.map((d) => (
                         <tr>
-                            <td>{d.id}</td>
-                            <td>{d.name}</td>
-                            <td>{d.price}</td>
+                            <td><Link to='/details'>{d.id}</Link></td>
+                            <td><>{d.name}</></td>
+                            <td><>{d.price}</></td>
                         </tr>
                     ))}
                 </tbody>
